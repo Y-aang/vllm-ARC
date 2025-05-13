056c7fad7dd4ef306f1863ad1ca742430e6ce08c
Y-aang <2386139091@qq.com>
2025-05-13 06:09:26 +0000

    try to fix ARC  before new version

--- vllm/core/customized_evictor.py
+++ vllm/core/customized_evictor.py
@@ -254,7 +254,6 @@ class CustomizedARCEvictor(Evictor):
         return (block_id in self.T1_free_table) or (block_id in self.T2_free_table)
 
     def evict(self, content_hash: int = None) -> Tuple[int, int]:
-        # print('len T1, T2', len(self.T1_free_table), len(self.T2_free_table), 'evict for:', content_hash)
         assert content_hash not in self.T1_free_table and content_hash not in self.T2_free_table
         # modify p
         if content_hash in self.B1_ghost:
@@ -273,31 +272,29 @@ class CustomizedARCEvictor(Evictor):
             evicted_block_id, evicted_content_hash = self._evict_from_T1()
         else:
             evicted_block_id, evicted_content_hash = self._replace(content_hash)
-        # print('evicted_content_hash:', evicted_content_hash)
-            
-        # Delete for_block from B1/B2 and record it for future add()
-        # print(f'{content_hash} in B1 B2', content_hash in self.B1_ghost, content_hash in self.B2_ghost)
 
         if content_hash in self.B1_ghost:
-            print('B1 hit', L1_size)
+            print('B1 hit', content_hash)
             self.B1_ghost.remove(content_hash)
-            # print(f'{content_hash} in B1 after remove:', content_hash in self.B1_ghost)
             self.removed_B1.append(content_hash)
         elif content_hash in self.B2_ghost:
-            print('B2 hit', L1_size)
+            print('B2 hit', content_hash)
             self.B2_ghost.remove(content_hash)
             self.removed_B2.append(content_hash)
         else:
             L1_size = len(self.T1_free_table) + len(self.evicted_T1) + len(self.B1_ghost)
-            L2_size = len(self.T2_free_table) + len(self.removed_T1) + len(self.removed_T2) + len(self.evicted_T2) + len(self.B2_ghost)
-            print('B miss, L1_size T1_total B1:', L1_size, len(self.T1_free_table) + len(self.removed_T1), len(self.B1_ghost))
+            L2_size = len(self.T2_free_table) + len(self.removed_T1) + len(self.removed_T2) + len(self.evicted_T2) + len(self.B2_ghost) + len(self.removed_B1) + len(self.removed_B2)
+            print(f'evict - len T1(R, E), T2(R, E), B1(R), B2(R) | {len(self.T1_free_table)} ({len(self.removed_T1)} {len(self.evicted_T1)}) {len(self.T2_free_table)} ({len(self.removed_T2)} {len(self.evicted_T2)})  | {len(self.B1_ghost)} ({len(self.removed_B1)}) {len(self.B2_ghost)} ({len(self.removed_B2)})  | p: {self.p} content_hash: {content_hash} L1_size {L1_size}')
+            # print('B miss, L1_size T1_total B1:', L1_size, len(self.T1_free_table) + len(self.removed_T1), len(self.B1_ghost))
             total_size = L1_size + L2_size
             # if L1_size >= self.max_size and len(self.T1_free_table) + len(self.removed_T1) < self.max_size and not self.B1_ghost:
             if L1_size >= self.max_size and self.B1_ghost:
-                print('B miss, popleft()')
-                self.B1_ghost.popleft()
+                while L1_size >= self.max_size and self.B1_ghost:
+                    poped_content_hash = self.B1_ghost.popleft()
+                    print('B miss, popleft(), hash:', poped_content_hash)
+                    L1_size -= 1
             elif L1_size < self.max_size and total_size > self.max_size:
-                if total_size == 2 * self.max_size and self.B2_ghost:
+                if total_size >= 2 * self.max_size and self.B2_ghost:
                     self.B2_ghost.popleft()
         
         return evicted_block_id, evicted_content_hash
@@ -312,6 +309,7 @@ class CustomizedARCEvictor(Evictor):
             self.evicted_T1.remove(block_id)
         if block_id in self.evicted_T2:
             self.evicted_T2.remove(block_id)
+        
         # Removed block: promote to T2
         if block_id in self.removed_T1 or block_id in self.removed_T2:
             if block_id in self.removed_T1:
@@ -326,7 +324,6 @@ class CustomizedARCEvictor(Evictor):
             self._cleanup_if_necessary(self.T2_priority_queue, self.T2_free_table)
             return
 
-        # print(f"{content_hash} in B1 or B2:", content_hash in self.B1_ghost, content_hash in self.B2_ghost)
         # assert content_hash not in self.B1_ghost and content_hash not in self.B2_ghost
         # Ghost hit in B1: promote to T2 and increase p
         if content_hash in self.removed_B1:
@@ -362,15 +359,15 @@ class CustomizedARCEvictor(Evictor):
 
     def remove(self, block_id: int):
         if block_id in self.T1_free_table:
+            print('remove - hit T1')
             content_hash = self.T1_free_table[block_id].content_hash
-            # print('remove hit T1:', content_hash)
             # assert content_hash not in self.B1_ghost
             
             self.T1_free_table.pop(block_id)
             self.removed_T1.append(block_id)
         elif block_id in self.T2_free_table:
+            print('remove - hit T2')
             content_hash = self.T2_free_table[block_id].content_hash
-            # print('remove hit T2:', content_hash)
             # assert content_hash not in self.B2_ghost
             
             self.T2_free_table.pop(block_id)
@@ -385,7 +382,7 @@ class CustomizedARCEvictor(Evictor):
     def _replace(self, content_hash_replace_for: int):
         Len_cached_T1 = len(self.T1_free_table) + len(self.evicted_T1)
         if self.T1_free_table and (
-            (content_hash_replace_for in self.B2_ghost and Len_cached_T1 == self.p) or
+            (content_hash_replace_for in self.B2_ghost and Len_cached_T1 >= self.p) or
             (Len_cached_T1 > self.p)
         ):
             block_id, content_hash = self._evict_from_T1()
@@ -421,8 +418,10 @@ class CustomizedARCEvictor(Evictor):
         """确保 ghost 列表 B1_ghost 和 B2_ghost 的大小不超过 max_size"""
         assert len(self.removed_B1) <= self.max_size and len(self.removed_B2) <= self.max_size
         while len(self.B1_ghost) + len(self.removed_B1) > self.max_size:
+            assert False
             self.B1_ghost.popleft()
         while len(self.B2_ghost) + len(self.removed_B2) > self.max_size:
+            assert False
             self.B2_ghost.popleft()
 
 
